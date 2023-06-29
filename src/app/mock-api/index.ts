06209f1b08a5e14b8fefb706b42f8e63f54044a4
Luchoragusa <luchito.rc4@gmail.com>
2023-06-28 23:06:53 -0300

    clean folders

--- src/app/mock-api/index.ts
+++ src/app/mock-api/index.ts
@@ -1,15 +1,9 @@
-import { AuthMockApi } from 'app/mock-api/common/auth/api';
 import { IconsMockApi } from 'app/mock-api/ui/icons/api';
 import { NavigationMockApi } from 'app/mock-api/common/navigation/api';
 import { NotificationsMockApi } from 'app/mock-api/common/notifications/api';
-import { UserMockApi } from 'app/mock-api/common/user/api';
-import { ProjectMockApi } from './dashboards/project/api';
 
 export const mockApiServices = [
-    AuthMockApi,
     IconsMockApi,
     NavigationMockApi,
-    NotificationsMockApi,
-    UserMockApi,
-    ProjectMockApi
+    NotificationsMockApi
 ];
